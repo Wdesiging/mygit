beba0a59e7ce30ebd4a0f69e9a82300e56ba95de
wojuedeok <929231670@qq.com>
2018-05-13 19:20:54 +0800

    XML

--- src/Data/RandomInstance.java
+++ src/Data/RandomInstance.java
@@ -7,7 +7,7 @@ public class RandomInstance {
 	private String instName;//工作流标记
 	private Parameters params;//生成实例具体参数
 	private String outputPath;//关键路径标记
-	
+	private int Job_MapNum;//执行数据量M
 	private Random rand = new Random(System.nanoTime());//生成随机实例
 	
 	public RandomInstance(Parameters p,String name){
@@ -23,7 +23,7 @@ public class RandomInstance {
     public Parameters getParams() {
 		return params;
 	}
-
+    private static final java.text.DecimalFormat df = new java.text.DecimalFormat("0.000");
 	public void setParams(Parameters params) {
 		this.params = params;
 	}
@@ -34,6 +34,9 @@ public class RandomInstance {
 
 	public String getInstName() {
 		return instName;
+	}
+	private static void printTo()throws Exception{
+		
 	}
 }
 
